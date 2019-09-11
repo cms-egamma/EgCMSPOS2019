94f2b08f51b7410aa0d741630ca17d854aab9559
Sam Harper <sam.j.harper@gmail.com>
2019-09-11 08:34:54 +0100

    twiki updates

---
+++ EgammaAnalysis/test/generateTwiki.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='prints E/gamma pat::Electrons/Photons us')
+#    parser.add_argument('body',help='body filename')
+    parser.add_argument('--ex1',help='ex1 filename')
+    parser.add_argument('--ex2',help='ex2 filename')
+    parser.add_argument('--ex3',help='ex3 filename')
+    args = parser.parse_args()
+
+    with open(args.ex1) as f:
+        ex1 = f.read()
+    with open(args.ex2) as f:
+        ex2 = f.read()
+    with open(args.ex3) as f:
+        ex3 = f.read()
+
+
+
+    body_txt="""
+---+ CMS Physics Object School 2019 in Aachen: E/gamma long exercise
+<noautolink> 
+Tutors: Sam James Harper and Swagata Mukherjee
+
+Electrons and photons play a crucial role in the success of CMS. They were vital in the discovery of the Higgs boson and play an important role in searches for new physics, including many models of supersymmetry. The design of the CMS detector, featuring a high-resolution electromagnetic calorimeter (ECAL) and an excellent tracker, provide clean identification and good energy and momentum resolution of these physics objects. In this exercise, we will examine electron and photon identification and study how these objects are used in physics analyses.
+
+The attendees are assumed to have basic knowledge of ROOT, C++, Python.
+
+Electrons and photons are particles which release most of their energy in the electromagnetic calorimeter (ECAL), so the main detector involved in their reconstruction is ECAL. However because of the tracker material in front of ECAL, electrons start radiating (bremsstrahlung) in many cases before reaching ECAL so special algorithms are involved to reconstruct electrons which radiates photons (electron tracks change curvature moving through the tracker layers) or photons which convert.
+
+Exercises are below:
+
+{ex1}
+
+{ex2}
+
+{ex3}
+
+ </noautolink>
+
+""".format(ex1=ex1,ex2=ex2,ex3=ex3)
+
+    print body_txt
